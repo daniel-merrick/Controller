bd95b949e31ec5886d92c3dc1c31bc11cd1d2dca
Daniel Merrick <dmerrick@purdue.edu>
2018-03-07 20:11:42 -0500

    Documenting

--- src/workermain.cpp
+++ src/workermain.cpp
@@ -7,12 +7,18 @@
 #include "../include/matSerial.hpp"
 #include <pthread.h>
 #include <thread>
-//This main fucntion is for testing read_viode() function
-
 
 using namespace std;
 using namespace cv;
 
+
+/******************************************
+* This is used to print (in this case) just
+* one image frame to a file. This function
+* is called inside a new thread from main 
+* in order to run asynchronously with the 
+* ServerUnit and CommUnit
+*******************************************/
 void printResults(StartTransport * s){
 // ADDED FOR VERIFICATION // REMOVE LATER	
 	int index = 0;
@@ -35,32 +41,39 @@ void printResults(StartTransport * s){
 	}
 }
 int main(int, char**){
-
-
 	//local ports for each ServerUnit to use
         static char local1_[5] = {'5','0','2','5','\0'};
-        //static char local2_[5] = {'9','9','9','3','\0'};
-        //static char local3_[5] = {'9','9','9','4','\0'};
 
         //host ports for each ClientUnit to connect to
         static char worker1_[5] = {'5','0','2','6','\0'};
-        //static char worker2_[5] = {'4','1','1','2','\0'};
-        //static char worker3_[5] = {'5','1','1','2','\0'};
 
         //host ip address
-
         static char workerIP1_[10] ={'1','2','7','.','0','.','0','.','1','\0'};
-        //static char workerIP2_[10] ={'1','2','7','.','0','.','0','.','1','\0'};
-        //static char workerIP3_[10] ={'1','2','7','.','0','.','0','.','1','\0'};
 
+	//fill a vector with structs of 'ConnectionInfo' which
+	//is given as a parameter to StartTransport() function
+	//The length of the vector(number of structs pushed)
+	//determines how many CommUnits are created.. One CommUnit 
+	//is made up of one ServerUnit and one ClientUnit
         std::vector<ConnectionInfo*> v;
         ConnectionInfo *temp = (ConnectionInfo *)malloc(sizeof(ConnectionInfo));
         temp -> hostIP_ = workerIP1_;
         temp -> hostPort_ = worker1_;
         temp -> localPort_ = local1_;
-
         v.push_back(temp);
-
+	
+	/*******************************************************
+	* If you want to run a function asynchronously 
+	* with the CommUnit, then you MUST call your
+	* function or functions inside a new thread, similar to
+	* how I call printResults.
+	********************************************************/
+	
+	//NOTE:
+		//StartTransport holds the 'inQueue' and 'outQueue', so if you want access to them, you must pass
+		//the object to your function parameter when calling your thread, similar to how call 'thread t2'
+			//'inQueue' is the data that this worker is receiving from the manager
+			//'outQueue' is queue that YOU must push to and want to be sent to the manager
 	StartTransport * s = new StartTransport(v);      
 	std::thread t1 = std::thread(&StartTransport::start, s);
 	std::thread t2 = std::thread(printResults, s);
@@ -68,5 +81,5 @@ int main(int, char**){
 	t2.join();
 	t1.join();
 
-
+	delete[] s;
 }
