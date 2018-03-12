fec0b83805da560da9f5c89420dfc06224bf69b9
Daniel Merrick <dmerrick@purdue.edu>
2018-03-11 20:32:09 -0400

    Documenting

--- src/managermain.cpp
+++ src/managermain.cpp
@@ -6,12 +6,16 @@
 #include "../include/CommUnit.hpp"
 #include <pthread.h>
 #include <thread>
-//This main fucntion is for testing read_viode() function
 
 
 using namespace std;
 using namespace cv;
-
+/*************************************
+* This is a function for testing 
+* purposes... it fills the 'outQueue'
+* with unsigned char* strings such as
+* '1', '2', ... '9999.'
+*************************************/
 static void testQueue(StartTransport * s){
 		int i = 0;
 		for ( i = 0; i < 10000; i++){
@@ -26,23 +30,23 @@ static void testQueue(StartTransport * s){
 		}
 
 }
+
 int main(int, char**){
 
 
-	//local ports for each ServerUnit to use
-	static char local1_[5] = {'5','0','2','6','\0'};
-	static char local2_[5] = {'5','0','2','7','\0'};
-	//static char local3_[5] = {'9','9','9','4','\0'};
+	//local ports for this managers ServerUnits to use
+	static char localPort1_[5] = {'5','0','2','6','\0'};
+	//static char localPort2_[5] = {'5','0','2','7','\0'};
+	//static char localPort3_[5] = {'9','9','9','4','\0'};
 
-	//host ports for each ClientUnit to connect to
-	static char worker1_[5] = {'5','0','2','8','\0'};
-	static char worker2_[5] = {'5','0','2','9','\0'};
-	//static char worker3_[5] = {'5','1','1','2','\0'};
+	//host ports for this managers ClientUnits to connect to
+	static char workerPort1_[5] = {'5','0','2','8','\0'};
+	//static char workerPort2_[5] = {'5','0','2','9','\0'};
+	//static char workerPort3_[5] = {'5','1','1','2','\0'};
 
-	//local ip address
-
+	//host ip address for this managers ClientUnits to connect to
 	static char workerIP1_[10] ={'1','2','7','.','0','.','0','.','1','\0'};
-	static char workerIP2_[10] ={'1','2','7','.','0','.','0','.','1','\0'};
+	//static char workerIP2_[10] ={'1','2','7','.','0','.','0','.','1','\0'};
 	//static char workerIP3_[10] ={'1','2','7','.','0','.','0','.','1','\0'};
 
 	std::vector<ConnectionInfo*> vectorOfConnections;
@@ -52,28 +56,26 @@ int main(int, char**){
 		return EXIT_FAILURE;
 	}
 	temp1 -> hostIP_ = workerIP1_;
-	temp1 -> hostPort_ = worker1_;
-	temp1 -> localPort_ = local1_;
+	temp1 -> hostPort_ = workerPort1_;
+	temp1 -> localPort_ = localPort1_;
 	
-	ConnectionInfo *temp2 = (ConnectionInfo *)malloc(sizeof(ConnectionInfo));
-	if(temp2 == NULL){
-		std::cout << "Malloc failed in Main" << std::endl;
-		return EXIT_FAILURE;
-	}
-	temp2 -> hostIP_ = workerIP2_;
-	temp2 -> hostPort_ = worker2_;
-	temp2 -> localPort_ = local2_;
-
 	vectorOfConnections.push_back(temp1);
-	vectorOfConnections.push_back(temp2);
 
 	//string filename = "1.mp4";
 	//int groupSize = 30;
 	//queue<vector<Mat>>* clips = new queue<vector<Mat>>();
+	
+	//Initial call to build StartTransport class, holds inQueue and outQueue
 	StartTransport * communicate = new StartTransport(vectorOfConnections);
-	std::thread t2 = std::thread (&testQueue, std::ref(communicate));
+	
+	//thread for filling StartTransport::outQueue, for testing purposes
+	//std::thread t2 = std::thread (&testQueue, std::ref(communicate));
+	
 	//Controller *controller = new Controller(3,clips,5,std::ref(communicate));
+	
+	//calls function StartTransport::start which builds all CommUnits 
 	std::thread t1 = std::thread (&StartTransport::start, std::ref(communicate));
+	
 	//std::thread t2 = std::thread (&Controller::start, std::ref(controller));
 	t1.join();
 	t2.join();
@@ -82,7 +84,7 @@ int main(int, char**){
 	delete[] communicate;
 	//delete[] controller;
 	free(temp1);
-	free(temp2);
-	//controller->start();
+	
+	//???
 	//controller->receive(msgs);
 }
